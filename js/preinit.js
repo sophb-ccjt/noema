aafaf9ba8c020019c3c28bf877c580c0dc4007b4
ccjt <circjit@gmail.com>
2026-01-31 17:48:06 -0300

    Add preinit.js for initial page setup and checks

---
+++ js/preinit.js
@@ -0,0 +1,107 @@
+// SIX SEVEN! SIX SEVEN! SIX SEVEN! SIX SEVEN! SIX SEVEN! SIX SEVEN! SIX SEVEN! SIX SEVEN!
+// 6767676767676767676767676767676767676767676767
+const isNode = typeof process !== 'undefined' && process.versions?.node;
+if (isNode) {
+    console.log("Node.js is not supported. Please use a browser.")
+    process.exit(1)
+}
+
+// localstorage definitions go here because this is the best place i could find
+// don't even try asking why
+if (localStorage.getItem('startup') === null) localStorage.startup = 'true'
+if (localStorage.getItem('pauseMusic') === null) localStorage.pauseMusic = 'true'
+if (localStorage.getItem('musicVolume') === null) localStorage.musicVolume = '0.18'
+if (localStorage.getItem('noShaders') === null && navigator.deviceMemory < 4) localStorage.noShaders = 'true'
+if (localStorage.getItem('noTransitions') === null && navigator.deviceMemory < 2) localStorage.noTransitions = 'true'
+
+
+
+let pageStart = performance.now(),
+    dependStart = null;
+
+console.log('Loading page...')
+let bgMusic
+document.addEventListener('DOMContentLoaded', () => {
+    dependStart = performance.now()
+    console.log(`Page loaded in ${performance.now() - pageStart}ms.`)
+    console.log('Loading resources...')
+    bgMusic = new Audio('./menu_music.flac')
+    bgMusic.preload = true
+    document.getElementById('clicktostart').textContent = 'loading page resources, please wait'
+})
+window.addEventListener('load', () => {
+    console.log(`Resources loaded in ${performance.now() - dependStart}ms.`)
+    console.log(`Finished loading in ${performance.now() - pageStart}ms!`)
+    if (test) { 
+        console.log(`Testing system...`)
+        document.getElementById('clicktostart').innerHTML = 'testing system...'
+        test()
+    }
+
+    const isMobile = navigator.userAgentData?.mobile === true || /Android|iPhone|iPad|iPod/i.test(navigator.userAgent) 
+    const supportChecks = [
+        {
+            trigger: isMobile,
+            warning: "Mobile is not supported. Please use a Desktop or Laptop computer."
+        },
+        {
+            trigger: document.documentMode,
+            warning: "Internet Explorer is not supported. Please use another browser like Chrome or Firefox."
+        }
+    ]
+    for (let i = 0; i < supportChecks.length; i++) {
+        const check = supportChecks[i]
+
+        if (check.trigger) {
+            document.getElementById('clicktostart').innerHTML = check.warning
+            return
+        }
+    }
+
+    if (localStorage.fromreboot === 'true') {
+        drawSpaghetti()
+        if (localStorage.startup === 'true') {
+            document.getElementById('clicktostart').innerHTML = 'starting...'
+            if (startup)
+                startup()
+            else
+                init()
+        } else {
+            document.getElementById('clicktostart').innerHTML = 'going to menu...'
+            document.getElementById('clicktostart').style.opacity = '0%'
+            init()
+        }
+    } else {
+        if (localStorage.startup === 'true')
+            document.getElementById('clicktostart').innerHTML = 'click or press enter to start'
+        else
+            document.getElementById('clicktostart').innerHTML = 'click or press enter to go to menu'
+
+        let start = () => {
+            document.onclick = document.onkeydown = null
+
+            drawSpaghetti()
+
+            if (localStorage.startup === 'true')
+                if (startup) {
+                    startup()
+                } else {
+                    init()
+                }
+
+            else {
+                document.getElementById('clicktostart').style.opacity = '0%'
+                setTimeout(() => {
+                    init()
+                }, 1e3)
+            }
+        }
+        document.onclick = start
+        document.onkeydown = (event) => {
+            if (event.key.toLowerCase() === "enter") {
+                start()
+            }
+        }
+    }
+    delete localStorage.fromreboot
+})
