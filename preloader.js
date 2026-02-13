e4ad1d54d4cdb88158faf9ab3bec8569e5cb9ef2
Soph B (ccjt) <148160919+ccjit@users.noreply.github.com>
2026-02-12 22:31:22 -0300

    minor cleanup and move script loading to preloader.js

---
+++ preloader.js
@@ -0,0 +1,72 @@
+const scripts = [
+    {
+        src: 'https://colorjs.io/dist/color.global.js'
+    },
+    {
+        src: 'https://code.jquery.com/jquery-3.7.1.js',
+        integrity: "sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=",
+        crossorigin: 'anonymous'
+    },
+    {
+        src: './js/modules/gamecontroller.min.js'
+    },
+    {
+        src: './js/modules/gapless5.js',
+        language: 'JavaScript',
+        type: 'text/javascript'
+    },
+    {
+        src: './js/modules/controller_input.js'
+    },
+    {
+        src: './js/modules/protoplus.js'
+    },
+    {
+        src: './js/modules/bgFormat.js'
+    },
+    {
+        src: './js/modules/URLutil.js'
+    },
+    {
+        src: './js/modules/file.js'
+    },
+    {
+        src: './js/base.js'
+    },
+    {
+        src: './js/error.js'
+    },
+    {
+        src: './js/power'
+    },
+    {
+        src: './js/game.js'
+    },
+    {
+        src: './js/controller.js'
+    },
+    {
+        src: './js/changelog.js'
+    },
+    {
+        src: './js/bg.js'
+    },
+    {
+        src: './js/ui.js'
+    },
+    {
+        src: './js/user.js'
+    },
+    {
+        src: './js/info.js'
+    }
+];
+
+scripts.forEach(obj => {
+    const script = document.createElement('script');
+    for (const [property, value] of Object.entries(obj)) {
+        script[property] = value;
+    }
+
+    document.body.appendChild(script);
+});
